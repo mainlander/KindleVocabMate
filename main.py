8c48c75d42c75e1c35004d77a4a08321b91ed31b
mainlander <mainlander1122@gmail.com>
2024-10-30 18:31:27 +0800

    Fix dbpathedit case problem

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ class KindleVocabMateMainWindow(QMainWindow, MainWindow.Ui_MainWindow):
         if platform.system() == 'Darwin':
             if os.path.exists('/volumes/kindle'):
                 if os.path.exists('/volumes/kindle/system/vocabulary/vocab.db'):
-                    self.dbpathedit.settext('/volumes/kindle/system/vocabulary/vocab.db')
+                    self.dbPathEdit.settext('/volumes/kindle/system/vocabulary/vocab.db')
                 else:
                     mbox = QMessageBox()
                     mbox.information(self, self.tr('no vocab.db'), self.tr('vocab.db does not exists in the kindle device.'))
@@ -239,7 +239,7 @@ class KindleVocabMateMainWindow(QMainWindow, MainWindow.Ui_MainWindow):
                     kindleDrive = drive.Caption
             if kindleDrive:
                 if os.path.exists(os.path.join(kindleDrive, 'system/vocabulary/vocab.db')):
-                    self.dbpathedit.settext('/volumes/kindle/system/vocabulary/vocab.db')
+                    self.dbPathEdit.settext('/volumes/kindle/system/vocabulary/vocab.db')
                 else:
                     mbox = QMessageBox()
                     mbox.information(self, self.tr('no vocab.db'), self.tr('vocab.db does not exists in the kindle device.'))
